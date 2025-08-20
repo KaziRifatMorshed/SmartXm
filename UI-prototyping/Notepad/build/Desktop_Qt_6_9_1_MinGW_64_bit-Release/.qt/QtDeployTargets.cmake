9960b88ba0542827ce300d907bba2ad8b73c2861
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2025-08-21 01:17:14 +0600

    dunno

---
+++ UI-prototyping/Notepad/build/Desktop_Qt_6_9_1_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Notepad_FILE C:/Users/Seam/Desktop/SmartXm-Cross-Platform-Desktop-Exam-Management-with-Integrated-Judge/UI-prototyping/Notepad/build/Desktop_Qt_6_9_1_MinGW_64_bit-Release/Notepad.exe)
+set(__QT_DEPLOY_TARGET_Notepad_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Notepad_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
