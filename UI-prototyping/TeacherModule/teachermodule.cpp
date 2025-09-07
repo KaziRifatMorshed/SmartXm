360d057cd13c0d8d3dcdd6e42726c22161ce8c90
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-09-08 01:42:06 +0600

    kisu ekta korsi

--- UI-prototyping/TeacherModule/teachermodule.cpp
+++ UI-prototyping/TeacherModule/teachermodule.cpp
@@ -1,33 +1,24 @@
 #include "teachermodule.h"
-#include "ui_teachermodule.h"
 #include "server.cpp"
+#include "ui_teachermodule.h"
 
 Server server;
 
 TeacherModule::TeacherModule(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::TeacherModule)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::TeacherModule) {
+  ui->setupUi(this);
 }
 
-TeacherModule::~TeacherModule()
-{
-    delete ui;
+TeacherModule::~TeacherModule() { delete ui; }
+
+void TeacherModule::on_StartServer_toolButton_clicked() {
+  if (server.getStatus() != "RUNNING") {
+    server.start();
+  }
 }
 
-void TeacherModule::on_StartServer_toolButton_clicked()
-{
-    if(server.getStatus() != "RUNNING"){
-        server.start();
-    }
+void TeacherModule::on_StopServer_toolButton_2_clicked() {
+  if (server.getStatus() == "RUNNING") {
+    server.stop();
+  }
 }
-
-
-void TeacherModule::on_StopServer_toolButton_2_clicked()
-{
-    if(server.getStatus() == "RUNNING"){
-        server.stop();
-    }
-}
-
