37a628134427847a24a06864d6db725b6605e7ca
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-09-17 16:12:46 +0600

    trying to install qsintella

---
+++ UI-prototyping/Notepad/build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Notepad_FILE /home/scr33nw0rmmj/Documents/GitHub/SmartXm-Cross-Platform-Desktop-Exam-Management-with-Integrated-Judge/UI-prototyping/Notepad/build/Desktop_Qt_6_9_1-Debug/Notepad)
+set(__QT_DEPLOY_TARGET_Notepad_TYPE EXECUTABLE)
