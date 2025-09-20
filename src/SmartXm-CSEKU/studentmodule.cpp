1d74c93e374a5bef2993ced87d350789d6e839a6
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-09-20 12:39:41 +0600

    Update studentmodule.cpp

--- src/SmartXm-CSEKU/studentmodule.cpp
+++ src/SmartXm-CSEKU/studentmodule.cpp
@@ -1,77 +1,86 @@
 #include "studentmodule.h"
 #include "ui_studentmodule.h"
-#include <iostream>
-#include <QTime>
 #include <QDesktopServices>
-#include <QUrl>
 #include <QDir>
+#include <QMessageBox>
 #include <QProcess>
+#include <QTime>
+#include <QUrl>
 #include <TerminalExecuter.h>
-
+#include <iostream>
 
 StudentModule::StudentModule(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::StudentModule)
-{
-    ui->setupUi(this);
-    ui->openRuleBook_pushButton->setDisabled(false);
+    : QMainWindow(parent), ui(new Ui::StudentModule) {
+  ui->setupUi(this);
+  ui->openRuleBook_pushButton->setDisabled(false);
 
-    // using a button
-    QPushButton *examTableActionBtn = new QPushButton("Enter");
-    ui->exam_list_tableWidget->setCellWidget(0,8, examTableActionBtn);
+  // using a button
+  QPushButton *examTableActionBtn = new QPushButton("Enter");
+  ui->exam_list_tableWidget->setCellWidget(0, 8, examTableActionBtn);
 }
 
-StudentModule::~StudentModule()
-{
-    delete ui;
+StudentModule::~StudentModule() { delete ui; }
+
+void StudentModule::on_exitBtn_profileTab_pushButton_2_clicked() { close(); }
+
+void showMsgBox(QWidget *parent, QString type, QString title, QString text) {
+  if (type == "information") {
+    QMessageBox::information(parent, title, text);
+  } else if (type == "warning") {
+    QMessageBox::warning(parent, title, text);
+  } else if (type == "critical") {
+    QMessageBox::critical(parent, title, text);
+  }
 }
 
-void StudentModule::on_exitBtn_profileTab_pushButton_2_clicked()
-{
-    close();
-}
+void StudentModule::on_openRuleBook_pushButton_clicked() {
+  showMsgBox(this, "information", "Rulebook", "Rulebook received! Click OK to open rulebook.");
+  std::cout << "Opening rulebook/instructions" << std::endl;
+  QString rulebookPath; // this needs to modify
+  // get a QMsgBox and then btn will be open
+  // open received rule book
 
-
-void StudentModule::on_openRuleBook_pushButton_clicked()
-{
-    std::cout << "Opening rulebook/instructions" << std::endl;
-    QString rulebookPath; // this needs to modify
-    // get a QMsgBox and then btn will be open
-    // open received rule book
-
-    // QString filePath = QDir::homePath() + "/a.pdf";
-    // std::cout << filePath.toStdString() << std::endl;
-    // if (!QFile::exists(filePath)) {
-    //     qDebug() << "File does not exist:" << filePath;
-    // } else {
-    //     QDesktopServices::openUrl(QUrl::fromLocalFile(filePath));
-    // }
-    QProcess pOpenFile;
+  // QString filePath = QDir::homePath() + "/a.pdf";
+  // std::cout << filePath.toStdString() << std::endl;
+  // if (!QFile::exists(filePath)) {
+  //     qDebug() << "File does not exist:" << filePath;
+  // } else {
+  //     QDesktopServices::openUrl(QUrl::fromLocalFile(filePath));
+  // }
+  QProcess pOpenFile;
 #ifdef __WIN32
 
 #elif __linux__
-    QProcess::startDetached("/usr/bin/microsoft-edge-stable", QStringList() << "/home/noobcod3r-rtx/a.pdf");
+  QProcess::startDetached("/usr/bin/microsoft-edge-stable",
+                          QStringList() << "/home/noobcod3r-rtx/a.pdf");
 
-/* DOES NOT WORK
-    // QProcess::startDetached("/usr/bin/xdg-open", QStringList() << "/home/noobcod3r-rtx/a.pdf");
-    // // system("/usr/bin/xdg-open /home/noobcod3r-rtx/Documents/a.pdf");
-    // pOpenFile.start("/usr/bin/xdg-open", QStringList() << "/home/noobcod3r-rtx/a.mp4");
-    // // pOpenFile.startDetached("ls");
-    // pOpenFile.waitForFinished();
-    // std::cout << "PRINTING OuTpUtS: " << pOpenFile.readAllStandardOutput().toStdString() << std::endl;
+  /* DOES NOT WORK
+      // QProcess::startDetached("/usr/bin/xdg-open", QStringList() <<
+     "/home/noobcod3r-rtx/a.pdf");
+      // // system("/usr/bin/xdg-open /home/noobcod3r-rtx/Documents/a.pdf");
+      // pOpenFile.start("/usr/bin/xdg-open", QStringList() <<
+     "/home/noobcod3r-rtx/a.mp4");
+      // // pOpenFile.startDetached("ls");
+      // pOpenFile.waitForFinished();
+      // std::cout << "PRINTING OuTpUtS: " <<
+     pOpenFile.readAllStandardOutput().toStdString() << std::endl;
 
-    // QDesktopServices::openUrl(QUrl::fromLocalFile("/home/noobcod3r-rtx/a.pdf"));
+      //
+     QDesktopServices::openUrl(QUrl::fromLocalFile("/home/noobcod3r-rtx/a.pdf"));
 
-    // const char* cmdd = "/usr/bin/xdg-open /home/noobcod3r-rtx/Documents/a.pdf";
-    // std::string s = termiExec(cmdd);
- */
+      // const char* cmdd = "/usr/bin/xdg-open
+     /home/noobcod3r-rtx/Documents/a.pdf";
+      // std::string s = termiExec(cmdd);
+   */
 
 #endif
 }
 
-void StudentModule::ruleBookReceived(){
-    QString currentTime = QTime::currentTime().toString();
-    ui->openRuleBook_pushButton->setDisabled(false);
-    ui->roolbook_receive_status_text_label->setText("Rulebook received from server at " + currentTime);
-    std::cout << "Rulebook received from server at " << currentTime.toStdString() << std::endl;
+void StudentModule::ruleBookReceived() {
+  QString currentTime = QTime::currentTime().toString();
+  ui->openRuleBook_pushButton->setDisabled(false);
+  ui->roolbook_receive_status_text_label->setText(
+      "Rulebook received from server at " + currentTime);
+  std::cout << "Rulebook received from server at " << currentTime.toStdString()
+            << std::endl;
 }
