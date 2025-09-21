65cbc9a4763fb892f371b7e709b741e01bb38064
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-09-22 00:53:14 +0600

    Revert "finishing"
    This reverts commit 009b21229874a65f861381d7002c261f6557f614.

--- src/SmartXm-CSEKU/studentmodule.cpp
+++ src/SmartXm-CSEKU/studentmodule.cpp
@@ -12,20 +12,18 @@
 StudentModule::StudentModule(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::StudentModule) {
   ui->setupUi(this);
-  ui->openRuleBook_pushButton->setDisabled(false); // after implementation, make it true
+  ui->openRuleBook_pushButton->setDisabled(false);
 
   // using a button
   QPushButton *examTableActionBtn = new QPushButton("Enter");
   ui->exam_list_tableWidget->setCellWidget(0, 8, examTableActionBtn);
-
-  connect(this, &StudentModule::fileReceived, this, &StudentModule::showMsgBox);
 }
 
 StudentModule::~StudentModule() { delete ui; }
 
 void StudentModule::on_exitBtn_profileTab_pushButton_2_clicked() { close(); }
 
-void _showMsgBox(QWidget *parent, QString type, QString title, QString text) { // currently useless
+void _showMsgBox(QWidget *parent, QString type, QString title, QString text) {
   if (type == "information") {
     QMessageBox::information(parent, title, text);
   } else if (type == "warning") {
@@ -34,8 +32,6 @@ void _showMsgBox(QWidget *parent, QString type, QString title, QString text) { /
     QMessageBox::critical(parent, title, text);
   }
 }
-
-/*
 void StudentModule::showMsgBox(QString type, QString title, QString text) {
     if (type == "information") {
         QMessageBox::information(this, title, text);
@@ -45,11 +41,6 @@ void StudentModule::showMsgBox(QString type, QString title, QString text) {
         QMessageBox::critical(this, title, text);
     }
 }
-*/
-
-void StudentModule::showMsgBox(const QString& msg) {
-    QMessageBox::information(this, "File Received", msg);
-}
 
 void StudentModule::on_openRuleBook_pushButton_clicked() {
   // showMsgBox("information", "Rulebook", "Rulebook received! Click OK to open rulebook."); // remove later
@@ -58,19 +49,40 @@ void StudentModule::on_openRuleBook_pushButton_clicked() {
   // get a QMsgBox and then btn will be open
   // open received rule book
 
+  // QString filePath = QDir::homePath() + "/a.pdf";
+  // std::cout << filePath.toStdString() << std::endl;
+  // if (!QFile::exists(filePath)) {
+  //     qDebug() << "File does not exist:" << filePath;
+  // } else {
+  //     QDesktopServices::openUrl(QUrl::fromLocalFile(filePath));
+  // }
   QProcess pOpenFile;
-
 #ifdef __WIN32
 
 #elif __linux__
-  QProcess::startDetached("./dependencies/qpdfjs/qpdfjs",
-                          QStringList() << "./examResources/rulebook.pdf");
+  QProcess::startDetached("/usr/bin/microsoft-edge-stable",
+                          QStringList() << "/home/noobcod3r-rtx/a.pdf");
+
+  /* DOES NOT WORK
+      // QProcess::startDetached("/usr/bin/xdg-open", QStringList() <<
+     "/home/noobcod3r-rtx/a.pdf");
+      // // system("/usr/bin/xdg-open /home/noobcod3r-rtx/Documents/a.pdf");
+      // pOpenFile.start("/usr/bin/xdg-open", QStringList() <<
+     "/home/noobcod3r-rtx/a.mp4");
+      // // pOpenFile.startDetached("ls");
+      // pOpenFile.waitForFinished();
+      // std::cout << "PRINTING OuTpUtS: " <<
+     pOpenFile.readAllStandardOutput().toStdString() << std::endl;
+
+      //
+     QDesktopServices::openUrl(QUrl::fromLocalFile("/home/noobcod3r-rtx/a.pdf"));
+
+      // const char* cmdd = "/usr/bin/xdg-open
+     /home/noobcod3r-rtx/Documents/a.pdf";
+      // std::string s = termiExec(cmdd);
+   */
 
 #endif
-}
-
-void StudentModule::showFileReceivedMsg(const QString& fileName) {
-    QMessageBox::information(this, "File Received", fileName + " was received!");
 }
 
 void StudentModule::ruleBookReceived() {
