46842cdbc2a2c6fe4fe46217fd239d54e4b03533
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-09-23 01:07:43 +0600

    unnecessary removed

--- UI-prototyping/Notepad/build/Desktop_Qt_6_9_2-Debug/.qt/deploy_Notepad_376077cc4c.cmake
+++
@@ -1,8 +0,0 @@
-include("/home/seam/Desktop/SmartXm/UI-prototyping/Notepad/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Notepad-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "/home/seam/Desktop/SmartXm/UI-prototyping/Notepad/build/Desktop_Qt_6_9_2-Debug/Notepad"
-    GENERATE_QT_CONF
-)
